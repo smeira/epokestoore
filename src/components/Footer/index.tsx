155b3d9fdce0a8e97370761bc35d44449f0e8581
smeira <samanta.meira@gmail.com>
2020-07-09 00:26:08 -0300

    novo commit

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,9 +4,12 @@ import { Linkedin, GitHub } from "react-feather";
 import S from "./Footer.module.css";
 
 const Footer = () => (
-  <footer className={S.FooterContainer}>   
-
+  <footer className={S.FooterContainer}>
+    <span className={S.FooterBlocks}>
+      Boas Compras!      
+    </span>    
   </footer>
 );
 
 export default Footer;
+
